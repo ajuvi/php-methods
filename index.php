aee46442be4e8474d2073f374cd9108dedb0a567
ajuvi <102821845+ajuvi@users.noreply.github.com>
2022-12-25 00:07:12 +0100

    artur

--- index.php
+++ index.php
@@ -1,10 +1,32 @@
 <?php
+//https://lornajane.net/posts/2008/accessing-incoming-put-data-from-php
+
+public printJson($text){
+    
+}
+
+public getContentHttp(){
+    parse_str(file_get_contents("php://input"),$vars);
+    return $vars;
+}
 
 
 $method = strtoupper($_SERVER['REQUEST_METHOD']);
 
-if($method==='GET'){
-    
+switch($method){
+    case "GET": 
+        message("Has escollit el mètode GET");
+        break;
+    case "POST": 
+        message("Has escollit el mètode POST");
+        break;
+    case "PUT": 
+        message("Has escollit el mètode PUT");            
+        break;
+    case "DELETE": 
+        message("Has escollit el mètode DELETE");            
+        break;
+    default:
+        message("Has escollit un mètode no conegut.")
 }
 
-public 

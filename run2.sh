85c8d314e3b2da6e05d13d234bcd2c466b12ac8a
ajuvi <102821845+ajuvi@users.noreply.github.com>
2022-12-25 10:46:50 +0100

    artur

---
+++ run2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+read -p "Entra un número: " n1
+read -p "Entra un altre número: " n2
+read -p "Entra una operació (+,-,*,/): " operacio
+
+if [ "$operacio" = "+" ]; then
+        #envia a través del mètode HTTP GET
+        curl "http://localhost/php-methods/rest.php?num1=${n1}&num2=${n2}"
+
+elif [ "$operacio" = "-" ]; then
+        #envia a través del mètode HTTP POST
+        curl -X POST http://localhost/php-methods/rest.php \
+             -H "Content-Type: application/json" \
+             -d "{\"num1\": $n1, \"num2\": $n2}"
+
+elif [ "$operacio" = "*" ]; then
+        #envia a través del mètode HTTP PUT
+        curl -X PUT http://localhost/php-methods/rest.php \
+             -H "Content-Type: application/json" \
+             -d "{\"num1\": $n1, \"num2\": $n2}"
+
+elif [ "$operacio" = "/" ]; then
+        #envia a través del mètode HTTP DELETE
+        curl -X DELETE http://localhost/php-methods/rest.php \
+             -H "Content-Type: application/json" \
+             -d "{\"num1\": $n1, \"num2\": $n2}"
+
+else
+        echo "ERROR: Has d'escriure una operació vàlida."
+
+fi
